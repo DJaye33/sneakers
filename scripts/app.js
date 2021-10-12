12dc719e117da2c00f85462530cecf7c32afe49d
Danny JeanLouis <dlouis3386@gmail.com>
2021-10-12 12:44:16 -0400

    add sidebar transition logic with overlay

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
+const overlay = document.querySelector(".overlay");
 const headerMenu = document.querySelector(".header__menu-btn");
 const iconClose = document.querySelector(".header__close");
 const navigation = document.querySelector(".header__nav");
 
-headerMenu.addEventListener("click", evt => {
-    navigation.classList.add("header-nav--show");
+headerMenu.addEventListener("click", (evt) => {
+  overlay.classList.add("overlay--active");
+  navigation.classList.add("header__nav--show");
 });
 
 iconClose.addEventListener("click", (evt) => {
-  console.log("clicked");
+  overlay.classList.toggle("overlay--active");
+  navigation.classList.toggle("header__nav--show");
 });
