f5e7f4a12430bf6a0ffb952a2813dcab23a102d0
Danny JeanLouis <dlouis3386@gmail.com>
2021-10-12 12:25:15 -0400

    add header nav UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
+const headerMenu = document.querySelector(".header__menu-btn");
+const iconClose = document.querySelector(".header__close");
+const navigation = document.querySelector(".header__nav");
 
+headerMenu.addEventListener("click", evt => {
+    navigation.classList.add("header-nav--show");
+});
+
+iconClose.addEventListener("click", (evt) => {
+  console.log("clicked");
+});
