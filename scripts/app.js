2e2b8dc9e7e777757d073b1053e438eba62c562b
Danny JeanLouis <dlouis3386@gmail.com>
2021-10-12 23:54:21 -0400

    add shopping cart

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,11 @@ const headerMenu = document.querySelector(".header__menu-btn");
 const iconClose = document.querySelector(".header__close");
 const navigation = document.querySelector(".header__nav");
 
+overlay.addEventListener("click", evt => {
+    evt.currentTarget.classList.remove("overlay--active");
+    navigation.classList.remove("header__nav--show");
+});
+
 headerMenu.addEventListener("click", (evt) => {
   overlay.classList.add("overlay--active");
   navigation.classList.add("header__nav--show");
