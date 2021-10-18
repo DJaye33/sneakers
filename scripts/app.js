8d1df1f5f5bc30f0e847c5b332e99468a6f5c3f0
Danny JeanLouis <dlouis3386@gmail.com>
2021-10-17 20:25:02 -0400

    add slideshow - in progress

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,13 @@ const overlay = document.querySelector(".overlay");
 const headerMenu = document.querySelector(".header__menu-btn");
 const iconClose = document.querySelector(".header__close");
 const navigation = document.querySelector(".header__nav");
+const headerCart = document.querySelector(".header__cart");
+const checkOut = document.querySelector(".checkout");
+const slidesMain = document.querySelectorAll(".main__slides");
 
-overlay.addEventListener("click", evt => {
-    evt.currentTarget.classList.remove("overlay--active");
-    navigation.classList.remove("header__nav--show");
+overlay.addEventListener("click", (evt) => {
+  evt.currentTarget.classList.remove("overlay--active");
+  navigation.classList.remove("header__nav--show");
 });
 
 headerMenu.addEventListener("click", (evt) => {
@@ -16,4 +19,16 @@ headerMenu.addEventListener("click", (evt) => {
 iconClose.addEventListener("click", (evt) => {
   overlay.classList.toggle("overlay--active");
   navigation.classList.toggle("header__nav--show");
+});
+
+headerCart.addEventListener("click", (evt) => {
+  checkOut.classList.toggle("checkout--show");
+});
+
+slidesMain.forEach((slide) => {
+  if (slide.classList.contains("main__slides--active")) {
+    slide.style.display = "block";
+  } else {
+    slide.style.display = "none";
+  }
 });
